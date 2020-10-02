7a7d3b91488c4d51926d43e066a8f3ef7c1239d4
Flavia Gramajo <flaviagramajo@gmail.com>
2020-10-02 18:05:09 +0200

    PlanList minor styling

--- src/components/PlansList.js
+++ src/components/PlansList.js
@@ -7,8 +7,9 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff',
-        alignItems: 'center',
+        alignItems: 'stretch',
         justifyContent: 'center',
+        width: '85%',
     }
 })
 
