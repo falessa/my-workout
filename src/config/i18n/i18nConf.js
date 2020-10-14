c95084d54406aec8150cf4d818c8d50f81385455
Flavia Gramajo <flaviagramajo@gmail.com>
2020-10-14 18:13:39 +0200

    i18n initial configuration

---
+++ src/config/i18n/i18nConf.js
@@ -0,0 +1,25 @@
+import i18n from 'i18next'
+import languageDetector from "i18next-browser-languagedetector";
+import { initReactI18next } from 'react-i18next'
+import enTranslation from './locale/en.json'
+import esTranslation from './locale/es.json'
+
+i18n
+    .use(languageDetector)
+    .use(initReactI18next)
+    .init({
+        resources: {
+            en: {
+                translation: enTranslation
+            },
+            es: {
+                translation: esTranslation
+            }
+        },
+        lng: "en",
+        fallbackLng: "en",
+
+        interpolation: {
+            escapeValue: false
+        }
+    })
