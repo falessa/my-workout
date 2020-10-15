5b70d4474d480b591840a27114d971796e0574a6
Flavia Gramajo <flaviagramajo@gmail.com>
2020-10-15 11:39:00 +0200

    Delete AppMainNavigator

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import '../src/config/i18n/i18nConf'
-// import AppMainNavigator from './navigation/AppMainNavigator';
 import BottomTabNavigator from './navigation/BottomTabNavigator'
 
 export const App = () => {
