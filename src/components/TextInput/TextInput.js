574063e8e088f06bb55deccbba87863993b2d17a
Flavia Gramajo <flaviagramajo@gmail.com>
2020-10-25 20:52:28 +0100

    New custom component: TextInput

---
+++ src/components/TextInput/TextInput.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, TextInput } from 'react-native';
+
+const styles = StyleSheet.create({
+    input: {
+        marginBottom: 8,
+        paddingHorizontal: 10,
+        height: 40,
+        borderColor: '#eee',
+        borderWidth: 2,
+        borderRadius: 20,
+        width: '60%'
+      },
+})
+
+const TextInputCustom = ({ onChangeText, placeholder='' }) => {
+    return ( 
+        <TextInput style={styles.input} onChangeText={onChangeText} placeholder={placeholder}/>    
+    )
+}
+
+export default TextInputCustom
---
+++ src/components/TextInput/index.js
@@ -0,0 +1,3 @@
+import TextInputCustom from './TextInput'
+
+export default TextInputCustom
