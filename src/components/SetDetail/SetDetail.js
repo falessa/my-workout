92e40722a9f8dcd4996e8deaf949b3d42aeddad6
Flavia Gramajo <flaviagramajo@gmail.com>
2020-11-19 20:02:46 +0100

    Simplify SetDetail component

--- src/components/SetDetail/SetDetail.js
+++ src/components/SetDetail/SetDetail.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import TextInput from '../TextInput';
+// import TextInput from '../TextInput';
 import TextSecondary from '../Text/TextSecondary/TextSecondary';
 import { useTranslation } from 'react-i18next';
 import { Ionicons } from '@expo/vector-icons';
@@ -12,12 +12,12 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         width: 120
     },
-    setDetailEmpty: {
-        alignItems: 'stretch',
-        justifyContent: 'center',
-        flexDirection: 'row',
-        width: 120
-    },
+    // setDetailEmpty: {
+    //     alignItems: 'stretch',
+    //     justifyContent: 'center',
+    //     flexDirection: 'row',
+    //     width: 120
+    // },
     setDetailInfo: {
         flexDirection: 'row',
         width: 120
@@ -28,20 +28,20 @@ const styles = StyleSheet.create({
     }
 })
 
-const SetDetail = ({ setDetail }) => {
+const SetDetail = ({ setDetail, onTrashPress, onChangeText }) => {
     const { t } = useTranslation()
 
-    const renderEmtpySetDetails = () => {
-        return (
-            <View style={styles.setDetailEmpty}>
-                <TextInput keyboardType='number-pad' placeholder={t('enterSetsPlaceholder')} style={styles.textInput}/>
-                <TextInput keyboardType='numeric' placeholder={t('enterRepetitionsPlaceholder')} style={styles.textInput}/>
-                <TextInput keyboardType='numeric' placeholder={t('enterKgsPlaceholder')} style={styles.textInput}/>
-                <Ionicons name="md-checkmark" size={26} color="black" style={{padding: 7}} onPress={()=> { console.log("confirm set detail") } }/>
-                <Ionicons name="md-trash" size={26} color="black" style={{padding: 7}} onPress={()=> { console.log("delete set detail") } }/>
-            </View>
-        )
-    }
+    // const renderEmtpySetDetails = () => {
+    //     return (
+    //         <View style={styles.setDetailEmpty}>
+    //             <TextInput keyboardType='number-pad' placeholder={t('enterSetsPlaceholder')} style={styles.textInput} onChangeText={onChangeText}/>
+    //             <TextInput keyboardType='numeric' placeholder={t('enterRepetitionsPlaceholder')} style={styles.textInput} onChangeText={onChangeText}/>
+    //             <TextInput keyboardType='numeric' placeholder={t('enterKgsPlaceholder')} style={styles.textInput} />
+    //             <Ionicons name="md-checkmark" size={26} color="black" style={{padding: 7}} onPress={()=> { console.log("confirm set detail") } }/>
+    //             <Ionicons name="md-trash" size={26} color="black" style={{padding: 7}} onPress={onTrashPress}/>
+    //         </View>
+    //     )
+    // }
 
     const renderSetDetailInfo = (setDetail) => {
         return (
@@ -56,9 +56,10 @@ const SetDetail = ({ setDetail }) => {
 
     return (
         <View style={styles.container}>
-            { setDetail ? 
+            { 
+                // setDetail ? 
                 renderSetDetailInfo(setDetail)
-                : renderEmtpySetDetails()
+                // : renderEmtpySetDetails()
             }
         </View>
     )
