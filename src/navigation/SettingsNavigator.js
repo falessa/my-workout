90ff35d2d149efec2beec3488dcbba9231e6f45e
Flavia Gramajo <flaviagramajo@gmail.com>
2020-11-25 15:31:42 +0100

    Help and About us screen components to Settings navigator

--- src/navigation/SettingsNavigator.js
+++ src/navigation/SettingsNavigator.js
@@ -2,8 +2,13 @@ import React from 'react';
 import { createStackNavigator } from "@react-navigation/stack";
 import SettingsScreen from '../screens/settings/SettingsScreen/SettingsScreen';
 import LanguageScreen from '../screens/settings/LanguageScreen/LanguageScreen';
-import SettingsScreenNavigationOptions from '../screens/settings/SettingsScreen/SettingsScreenNavigationOptions'
-import LanguageScreenNavigationOptions from '../screens/settings/LanguageScreen/LanguageScreenNavigationOptions'
+import HelpScreen from '../screens/settings/HelpScreen/HelpScreen';
+import AboutUsScreen from '../screens/settings/AboutUsScreen/AboutUsScreen';
+import SettingsScreenNavigationOptions from '../screens/settings/SettingsScreen/SettingsScreenNavigationOptions';
+import LanguageScreenNavigationOptions from '../screens/settings/LanguageScreen/LanguageScreenNavigationOptions';
+import HelpScreenNavigationOptions from '../screens/settings/HelpScreen/HelpScreenNavigationOptions';
+import AboutUsScreenNavigationOptions from '../screens/settings/AboutUsScreen/AboutUsScreenNavigationOptions';
+
 import { useTranslation } from 'react-i18next';
 
 const Stack = createStackNavigator();
@@ -18,6 +23,8 @@ function SettingsNavigator() {
         <Stack.Navigator>
             <Stack.Screen name='Settings' options={SettingsScreenNavigationOptions(t('settingsScreenTitle'))} component={SettingsScreen}/>
             <Stack.Screen name='LanguageScreen' options={LanguageScreenNavigationOptions(t('languageScreenTitle'))} component={LanguageScreen} />
+            <Stack.Screen name='HelpScreen' options={HelpScreenNavigationOptions(t('helpScreenTitle'))} component={HelpScreen} />
+            <Stack.Screen name='AboutUsScreen' options={AboutUsScreenNavigationOptions(t('aboutUsScreenTitle'))} component={AboutUsScreen} />
         </Stack.Navigator>
     )
 }
