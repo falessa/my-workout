ef1f65410d454a1f72c5a04bffa1e12a6efae6e1
Flavia Gramajo <flaviagramajo@gmail.com>
2023-04-20 18:21:56 +0200

    Update snapshots, and fix some tests

--- src/components/ListMenuOptionSelectable/__tests__/ListMenuOptionSelectable.test.js
+++ src/components/ListMenuOptionSelectable/__tests__/ListMenuOptionSelectable.test.js
@@ -1,20 +1,33 @@
 import "react-native";
 import React from "react";
 import { create } from "react-test-renderer";
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
 import ListMenuOptionSelectable from '../ListMenuOptionSelectable';
 
-describe.skip('ListMenuOptionSelectable tests', () => {
+describe('ListMenuOptionSelectable tests', () => {
     const props = {
         options: [
             "Selectable Option 1",
             "Selectable Option 2",
             "Selectable Option 3"
         ]
-    }
+    };
 
-    const wrapper = create(<ListMenuOptionSelectable {...props} />)
+    const mockStore = configureStore([]);
+    const store = mockStore({
+        settings: {
+            language: 'en'
+        }
+    });
+
+    const wrapper = create(
+        <Provider store={store}>
+            <ListMenuOptionSelectable {...props} />
+        </Provider>
+    );
 
     test('Render ListMenuOptionSelectable', () => {
         expect(wrapper).toMatchSnapshot()
-    })
-})
+    });
+});
